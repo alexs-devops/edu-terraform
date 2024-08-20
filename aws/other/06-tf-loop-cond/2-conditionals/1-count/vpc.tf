4e4b2cfe55d08c47e951f9c09e1090f3821b27a5
as511-devops <80338288+as511-devops@users.noreply.github.com>
2024-08-20 18:13:53 +0300

    edu-terraform: terragrant

--- aws/other/06-tf-loop-cond/2-conditionals/1-count/vpc.tf
+++ aws/other/06-tf-loop-cond/2-conditionals/1-count/vpc.tf
@@ -3,3 +3,25 @@ module "vpcs" {
 
   enable_database_vpc = true
 }
+
+/* resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main"
+  } 
+}
+
+variable "enable_database_vpc" {
+  default = true
+}
+
+resource "aws_vpc" "db" {
+  count = var.enable_database_vpc ? 1 : 0
+
+  cidr_block = "10.1.0.0/16"
+
+  tags = {
+    Name = "db"
+  }
+} */
